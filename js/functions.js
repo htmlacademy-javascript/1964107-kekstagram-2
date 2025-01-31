baba802e4cad56abbf9553f618b0dfff42b76af3
chepelova <168730690+chepelova@users.noreply.github.com>
2025-01-31 13:31:56 +0300

    доработки домашней работы

--- js/functions.js
+++ js/functions.js
@@ -1,38 +1,32 @@
-/*Функция для проверки длины строки. Она принимает строку, которую нужно проверить, и максимальную длину и возвращает true,
-если строка меньше или равна указанной длине, и false, если строка длиннее. Эта функция
-нам пригодится для валидации формы. Примеры использования функции: */
+const compareStrLength = (stroke, maxSymbol) => stroke.length <= maxSymbol;
+compareStrLength('ss', 2);
 
-
-const compareStroke = (line, symbol) => line.length <= symbol.length;
-
-compareStroke();
-
-/*Функция для проверки, является ли строка палиндромом. Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.*/
 const isPalindrom = (stroke) => {
-  stroke = stroke.replaceAll(' ','');
-  stroke = stroke.toLowerCase();
+  const formattedStr = stroke.replaceAll(' ','').toLowerCase();
   let result = '';
-  for (let i = stroke.length - 1; i >= 0 ; i--) {
-    result += stroke[i];
+
+  for (let i = formattedStr.length - 1; i >= 0 ; i--) {
+    result += formattedStr[i];
   }
-  return stroke === result;
+
+  return formattedStr === result;
 };
 
 isPalindrom('123');
 
-/*Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа. Если в строке нет ни одной цифры, функция должна вернуть NaN: */
-
-const removeNumber = (string) => {
+const removeNumber = (stroke) => {
+  const formattedStr = stroke.toString();
   let result = '';
-  string = string.toString();
-  for (let i = 0; i <= string.length - 1 ; i++) {
-    if (Number.isNaN(parseInt(string[i], 10)) === false){
-      result += string[i];
+  for (let i = 0; i <= formattedStr.length - 1 ; i++) {
+    if (Number.isNaN(parseInt(formattedStr[i], 10)) === false){
+      result += formattedStr[i];
     }
   }
+
   if (result === '') {
     return NaN;
   }
+
   return Number(result);
 };
 
