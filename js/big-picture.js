5393bbb64b50dd190b4e5075f4276561a1adf0dc
chepelova <168730690+chepelova@users.noreply.github.com>
2025-03-20 23:02:58 +0300

    правки 8 раздел

--- js/big-picture.js
+++ js/big-picture.js
@@ -2,19 +2,18 @@ const bigPicture = document.querySelector('.big-picture');
 const bigPictureClose = document.querySelector('#picture-cancel');
 const container = document.querySelector('.pictures');
 const body = document.querySelector('body');
-const bigImg = bigPicture.querySelector('.big-picture__img');
 const likesCoint = bigPicture.querySelector('.likes-count');
 const bigDescription = bigPicture.querySelector('.social__caption');
-const socialHidden = bigPicture.querySelector('.social__comment-count');
-const commentHidden = bigPicture.querySelector('.comments-loader');
-//const commentNumber = bigImg.querySelector('.social__comment-total-count');
+//const socialHidden = bigPicture.querySelector('.social__comment-count');
+//const commentHidden = bigPicture.querySelector('.comments-loader');
+const commentNumber = bigPicture.querySelector('.social__comment-total-count');
 
 const fillModal = (photo) => {
   // TODO заполнение модалки
-  bigImg.src = photo.url;
+  bigPicture.querySelector('.big-picture__img img').src = photo.url;
   likesCoint.textContent = photo.likes;
   bigDescription.textContent = photo.description;
-  //commentNumber.textContent = photo.comment.length?;
+  commentNumber.textContent = photo.comment.length;
 
   //console.log(photo);
 };
@@ -24,8 +23,8 @@ const openModalPicture = (photos) => {
   container.addEventListener('click', (evt) => {
     if (evt.target.matches('img')) {
       bigPicture.classList.remove('hidden');
-      socialHidden.classList.add('hidden');
-      commentHidden.classList.add('hidden');
+      //socialHidden.classList.add('hidden');
+      //commentHidden.classList.add('hidden');
       body.classList.add('modal-open');
       const idThumbnails = Number(evt.target.closest('a').dataset.id);
       const photo = photos.find(({id}) => id === idThumbnails);
@@ -41,11 +40,13 @@ const closeModalPicture = () => {
       evt.preventDefault();
       bigPicture.classList.add('hidden');
     }
+    body.classList.remove('modal-open');
   });
 
   bigPictureClose.addEventListener('click', () => {
     bigPicture.classList.add('hidden');
   });
+  body.classList.remove('modal-open');
 };
 
 export { openModalPicture, closeModalPicture};
