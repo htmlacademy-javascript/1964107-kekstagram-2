af7318a1015d41b48af9ebbfd521dfc78c269fe0
chepelova <168730690+chepelova@users.noreply.github.com>
2025-04-01 14:28:38 +0300

    добавляет валюдацию хештегов

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 import { createPhotos } from './create-array-photo.js';
 import { renderThumbnails } from './thumbnails.js';
 import { initPreviewPictures } from './big-picture.js';
-import { initUploadPicture } from './upload-picture.js';
+import { initUploadForm } from './form/upload-form.js';
 
 const photos = createPhotos();
 renderThumbnails(photos);
 initPreviewPictures(photos);
-initUploadPicture();
+initUploadForm();
